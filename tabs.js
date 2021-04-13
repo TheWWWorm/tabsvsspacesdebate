b76dea9f5992ec0872c4ff3e68525da3bbc07b81
Deniss <thewwworm@gmail.com>
2021-04-14 02:30:51 +0300

    Add files via upload

---
+++ tabs.js
@@ -0,0 +1,23 @@
+function spaces() {
+	if (true === true) {
+		if (false === false) {
+			if (true === true) {
+				if (false === false) {
+					if (true === true) {
+						if (false === false) {
+							if (true === true) {
+								if (false === false) {
+									if (true === true) {
+										if (false === false) {
+											return 42;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
